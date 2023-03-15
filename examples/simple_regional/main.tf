20c6ac4cca2c429a05122b5d2ceda18d406f2900
chrisahsiong23 <chris.ah-siong@intel.com>
2023-03-15 09:41:28 -0700

    simplified example, readme in process

--- examples/simple_regional/main.tf
+++ examples/simple_regional/main.tf
@@ -16,6 +16,16 @@
 
 locals {
   cluster_type = "simple-regional"
+  project_id = "intel-csa-resource-gcp"
+  cluster_name_suffix = "-1"
+  region = "us-east1"
+  network = "default"
+  subnetwork = "default"
+  ip_range_pods = "k8s-pods"
+  ip_range_services = "k8s-services"
+  compute_engine_service_account = "551221341017-compute@developer.gserviceaccount.com"
+  enable_binary_authorization = false
+  skip_provisioners = false
 }
 
 data "google_client_config" "default" {}
@@ -28,17 +38,24 @@ provider "kubernetes" {
 
 module "gke" {
   source                      = "terraform-google-modules/kubernetes-engine/google"
-  project_id                  = var.project_id
-  name                        = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
+  project_id                  = local.project_id
+  name                        = "${local.cluster_type}-cluster${local.cluster_name_suffix}"
   regional                    = true
-  region                      = var.region
-  network                     = var.network
-  subnetwork                  = var.subnetwork
-  ip_range_pods               = var.ip_range_pods
-  ip_range_services           = var.ip_range_services
+  region                      = local.region
+  network                     = local.network
+  subnetwork                  = local.subnetwork
+  ip_range_pods               = local.ip_range_pods
+  ip_range_services           = local.ip_range_services
   create_service_account      = false
-  service_account             = var.compute_engine_service_account
+  service_account             = local.compute_engine_service_account
   enable_cost_allocation      = true
-  enable_binary_authorization = var.enable_binary_authorization
-  skip_provisioners           = var.skip_provisioners
+  enable_binary_authorization = local.enable_binary_authorization
+  skip_provisioners           = local.skip_provisioners
+
+  node_pools = [
+    {
+      name = "my-pool"
+      machine_type = "n2-standard-48"
+    }
+  ] 
 }
